2ef6bcaf3f8bc526f174b8dd3e437b2bc9480c89
kurealnum <cattledoger@gmail.com>
2024-07-14 14:55:18 -0400

    QOL updates

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import requests
 
+OpenMeteoURL = "https://api.open-meteo.com/v1/forecast"
+
 
 def home(request):
     return render(request, "home.html")
@@ -9,19 +11,28 @@ def home(request):
 def results(request):
     latitude = request.POST.get("latitude")
     longitude = request.POST.get("longitude")
+    city = request.POST.get("city")
 
     params = {
         "latitude": latitude,
         "longitude": longitude,
-        "current": ["temperature_2m"],
+        "current": ["temperature_2m", "wind_speed_10m", "relative_humidity_2m"],
+        "daily": ["sunrise", "sunset"],
     }
 
     response = requests.get(OpenMeteoURL, params=params).json()
 
     data = {
+        "name": city,
         "latitude": response["latitude"],
         "longitude": response["longitude"],
         "temperature": response["current"]["temperature_2m"],
+        "sunrise": response["daily"]["sunrise"][0].split("T")[
+            1
+        ],  # getting the exact time, the API gives us a jumble of info
+        "sunset": response["daily"]["sunset"][0].split("T")[1],
+        "humidity": response["current"]["relative_humidity_2m"],
+        "wind_speed": response["current"]["wind_speed_10m"],
     }
 
     return render(request, "results.html", context={"data": data})
