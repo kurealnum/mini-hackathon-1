8ad9ab8542a35fbe31c395ff552545d04adbea67
kurealnum <cattledoger@gmail.com>
2024-07-14 13:26:18 -0400

    Completely removed the OpenMeteo python package

--- weather/views.py
+++ weather/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-import openmeteo_requests
-from openmeteo_sdk.Variable import Variable
 import requests
-
-OpenMeteoClient = openmeteo_requests.Client()
-OpenMeteoURL = "https://api.open-meteo.com/v1/forecast"
 
 
 def home(request):
@@ -21,28 +16,12 @@ def results(request):
         "current": ["temperature_2m"],
     }
 
-    response = OpenMeteoClient.weather_api(
-        "https://api.open-meteo.com/v1/forecast", params=params
-    )[0]
-    # the current weather data
-    current = response.Current()
-
-    # this is all from here: https://github.com/open-meteo/python-requests/blob/main/README.md
-    current_variables = list(
-        map(lambda i: current.Variables(i), range(0, current.VariablesLength()))  # type: ignore
-    )
-
-    current_temperature_2m = next(
-        filter(
-            lambda x: x.Variable() == Variable.temperature,  # type: ignore
-            current_variables,
-        )
-    )
+    response = requests.get(OpenMeteoURL, params=params).json()
 
     data = {
-        "latitude": response.Latitude(),
-        "longitude": response.Longitude(),
-        "temperature": current_temperature_2m.Value(),  # type: ignore
+        "latitude": response["latitude"],
+        "longitude": response["longitude"],
+        "temperature": response["current"]["temperature_2m"],
     }
 
     return render(request, "results.html", context={"data": data})
