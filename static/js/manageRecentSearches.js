bcc0d033c6b5c1dbc6d76725356fcc2883f62340
kurealnum <cattledoger@gmail.com>
2024-07-14 15:47:42 -0400

    Added JS stuff, moved files around

---
+++ static/js/manageRecentSearches.js
@@ -0,0 +1,32 @@
+const current = JSON.parse(localStorage.getItem("searches"));
+
+const recentSearches = document.getElementById("recent-searches");
+const searchCityButton = document.getElementById("search-city");
+
+if (!current) {
+  recentSearches.innerHTML = "<p>No recent searches</p>";
+} else {
+  recentSearches.innerHTML = current
+    .map((search) => "<p>" + search + "</p>")
+    .join(" ");
+}
+
+searchCityButton.addEventListener("click", updateLocalStorage);
+
+function updateLocalStorage() {
+  const newEntry = document.getElementById("location").value;
+  let current = JSON.parse(localStorage.getItem("searches"));
+  console.log(current, newEntry);
+  if (current && current.length >= 5) {
+    current.pop();
+  }
+  if (current) {
+    current.unshift(newEntry);
+  } else {
+    current = [newEntry];
+  }
+  localStorage.setItem("searches", JSON.stringify(current));
+  recentSearches.innerHTML = current
+    .map((search) => "<p>" + search + "</p>")
+    .join(" ");
+}
